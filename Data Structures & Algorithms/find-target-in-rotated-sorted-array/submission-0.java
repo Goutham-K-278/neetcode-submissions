b69f8996d093da915dc0a8c2b9a1d38bc8e5511c
Goutham K <goutham02272006@gmail.com>
2026-08-21 21:41:05 +0530

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        return map.getOrDefault(target, -1);
+    }
+}
